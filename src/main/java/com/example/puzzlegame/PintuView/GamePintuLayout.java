5bcc75768a425285d01c3fbf075b39506910f862
BestJerry <jerrywu.aiesec.scau@gmail.com>
2016-09-10 18:49:12 +0800

    实现交换图片动画功能.

--- src/main/java/com/example/puzzlegame/PintuView/GamePintuLayout.java
+++ src/main/java/com/example/puzzlegame/PintuView/GamePintuLayout.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -86,7 +88,7 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
         mItemWidth = (mwidth - mPadding * 2 - mMagin * (mColumn - 1)) / mColumn;
         mGamepintuItems = new ImageView[mColumn * mColumn];
 
-//生成item，设置rule
+        //生成item，设置rule
         for (int i = 0; i < mGamepintuItems.length; i++) {
             ImageView item = new ImageView(getContext());
             item.setOnClickListener(this);
@@ -107,7 +109,7 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
                 lp.addRule(RelativeLayout.RIGHT_OF, mGamepintuItems[i - 1].getId());
             }
 
-//如果不是第一行,谁知topmargin和rule
+            //如果不是第一行,谁知topmargin和rule
             if ((i + 1) > mColumn) {
                 lp.topMargin = mMagin;
                 lp.addRule(RelativeLayout.BELOW, mGamepintuItems[i - mColumn].getId());
@@ -159,6 +161,9 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+        if (isaniming) {
+            return;
+        }
         //两次点击同一个item，取消选择
         if (mfirst == view) {
             mfirst.setColorFilter(null);
@@ -177,24 +182,120 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     }
 
+    //动画层
+    private RelativeLayout manimlayout;
+    private boolean isaniming;
+
+
     //交换item
     private void exchangerview() {
+
+        //取消高亮
         mfirst.setColorFilter(null);
-        String firsttag = (String) mfirst.getTag();
-        String secondtag = (String) msecond.getTag();
 
-        String[] firstparams = firsttag.split("_");
-        String[] secondparams = secondtag.split("_");
+        setupanimlayout();
 
-        Bitmap firstbitmap = mitembitmaps.get(Integer.parseInt(firstparams[0])).getBitmap();
+        ImageView first = new ImageView(getContext());
+        final Bitmap firstbitmap = mitembitmaps.get(getimageidbytag((String) mfirst.getTag())).getBitmap();
 
-        Bitmap secondbitmap = mitembitmaps.get(Integer.parseInt(secondparams[0])).getBitmap();
-        msecond.setImageBitmap(firstbitmap);
-        mfirst.setImageBitmap(secondbitmap);
+        first.setImageBitmap(firstbitmap);
+        LayoutParams lp = new LayoutParams(mItemWidth, mItemWidth);
+        lp.leftMargin = mfirst.getLeft() - mPadding;
+        lp.topMargin = mfirst.getTop() - mPadding;
+        first.setLayoutParams(lp);
+        manimlayout.addView(first);
 
-        mfirst.setTag(secondtag);
-        msecond.setTag(firsttag);
-        mfirst = msecond = null;
 
+        ImageView second = new ImageView(getContext());
+        final Bitmap secondbitmap = mitembitmaps.get(getimageidbytag((String) msecond.getTag())).getBitmap();
+
+        second.setImageBitmap(secondbitmap);
+        LayoutParams lp2 = new LayoutParams(mItemWidth, mItemWidth);
+        lp2.leftMargin = msecond.getLeft() - mPadding;
+        lp2.topMargin = msecond.getTop() - mPadding;
+        second.setLayoutParams(lp2);
+        manimlayout.addView(second);
+
+
+        //设置动画
+        TranslateAnimation anim = new TranslateAnimation(0, msecond.getLeft() - mfirst.getLeft(), 0,
+                msecond.getTop() - mfirst.getTop());
+        anim.setDuration(300);
+        anim.setFillAfter(true);
+        first.startAnimation(anim);
+
+        TranslateAnimation animsecond = new TranslateAnimation(0, -msecond.getLeft() + mfirst.getLeft(), 0,
+                -msecond.getTop() + mfirst.getTop());
+        animsecond.setDuration(300);
+        animsecond.setFillAfter(true);
+        second.startAnimation(animsecond);
+
+        //监听动画
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+                //隐藏原来的图片
+                mfirst.setVisibility(View.INVISIBLE);
+                msecond.setVisibility(View.INVISIBLE);
+                isaniming = true;
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                String firsttag = (String) mfirst.getTag();
+                String secondtag = (String) msecond.getTag();
+
+
+                msecond.setImageBitmap(firstbitmap);
+                mfirst.setImageBitmap(secondbitmap);
+
+                mfirst.setTag(secondtag);
+                msecond.setTag(firsttag);
+                //显示交换后的图片
+                mfirst.setVisibility(View.VISIBLE);
+                msecond.setVisibility(View.VISIBLE);
+
+                mfirst = msecond = null;
+                //移除动画层
+                manimlayout.removeAllViews();
+                isaniming = false;
+
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+    }
+
+    //根据tag获取id
+    public int getimageidbytag(String tag) {
+
+        String[] split = tag.split("_");
+        return Integer.parseInt(split[0]);
+
+
+    }
+
+    public int getimageindex(String tag) {
+        String[] split = tag.split("_");
+        return Integer.parseInt(split[1]);
+    }
+
+    //构造动画层
+    private void setupanimlayout() {
+
+        if (manimlayout == null) {
+            manimlayout = new RelativeLayout(getContext());
+            addView(manimlayout);
+
+        }
     }
 }
